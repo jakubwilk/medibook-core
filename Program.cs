b04e901c695a0177ffcb7c00119626b1398f2045
jakubwilk <jakub.wilk@jakubwilk.pl>
2024-03-10 11:57:43 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "mediBook API Documentation", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(swagger => swagger.SwaggerEndpoint("/swagger/v1/swagger.json", "mediBook API Documentation v1"));
+    app.UseReDoc(doc =>
+    {
+        doc.RoutePrefix = "docs";
+        doc.DocumentTitle = "mediBook API Documentation";
+        doc.SpecUrl = "/swagger/v1/swagger.json";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
